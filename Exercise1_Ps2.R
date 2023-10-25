075910af4396bd3c676eb12ef85f766a3c33a1c1
Michelle Papadakis <papadakismichelle@gmail.com>
2023-10-25 17:29:43 +0100

    Adding arguments to the function through the dev branch

--- Exercise1_Ps2.R
+++ Exercise1_Ps2.R
@@ -1,24 +1,23 @@
 ## Author: Michelle Papadakis Barradas
 ## Exercise 1 of Problem set 2
-
-# Instructions: Defining a function with a single argument data, 
-# that takes a dataset and performs some input transformation on it. 
 library("tidyverse")
-# Using  built in data base called "band_instruments"
-
-add_phrase_column <- function(data) {
-  # Combinar nombre e instrumento en la columna "phrase"
+# Creating a function and using  built in data base called "band_instruments"
+add_phrase_column <- function(data, uppercase = FALSE) {
+  # To combine the name and the instrument in a new column
   data$phrase <- paste(data$name, "plays the", data$plays)
-  return(data)  # Devolver el data frame con la columna "phrase" agregada
+  if (uppercase) {
+    data$name <-toupper(data$name)
+  }
+  return(data)  
 }
-
-# Ejemplo de uso:
+#Example:
 library("tidyverse")
 data("band_instruments")
 band_instruments <- as.data.frame(band_instruments)
 
-# Llamar a la función para agregar la columna de frase
-band_instruments_with_phrase <- add_phrase_column(band_instruments)
+#Calling the function
+phrase_uppercase <- add_phrase_column(band_instruments, uppercase = TRUE)
 
-# Imprimir el resultado
-print(band_instruments_with_phrase)
+# Print the result
+print(phrase_uppercase)
+
